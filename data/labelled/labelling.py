669f3f533a343b9ca4dfdd60ef3cc2059ebb8dc0
Terry <zhaterry@gmail.com>
2023-08-31 18:12:28 -0400

    updated the labelling script

--- data/labelled/labelling.py
+++ data/labelled/labelling.py
@@ -1,45 +1,82 @@
 import json
+import os
 import openai
 
-API_KEY = ''
+def label(api_key: str, label_folder: str, main_instruction: str):
+    useless_comments = set(['No Positive', 'No Negative', 'All', 'Everything', 'everything', 'Nothing', 'nothing'])
 
+    label_dir = os.path.join(os.getcwd(), 'data', 'labelled', label_folder)
+    comment_list: list[str] = []
 
-def main():
-    openai.api_key = API_KEY
+    for file_name in os.listdir(label_dir):
+        if file_name == 'labelled.json': continue
+        with open(os.path.join(label_dir, file_name), 'r') as comments_json:
+            for comment in json.load(comments_json):
+                if comment in useless_comments: continue
+                comment_list.append(f"\"{comment}\"")
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": 
-                    "You are an assistant who helps me label comments of hotels in the following 8 aspects: \n" + 
-                    "- Cleanliness\n"+
-                    "- Food\n"+
-                    "- Service and Staff\n"+
-                    "- Amenities\n"+
-                    "- Location\n"+
-                    "- Room Comfort\n"+
-                    "- Parking\n"+
-                    "- Security\n"+
-                    "You will rate each aspect from 0 to 1, with 0 being terrible and 1 being amazing. If the aspect is no mentioned, use 0.5.\n"+
-                    "I will give you a comma-delimited list of comments and I want you to rate each comment and respond in the json format with a list of json object.\n"+
-                    "Each json object should have a field called \"comment\" which contains the full comment and a field calld \"labels\" which should be a json object containing the rating of all 8 aspects"
-            },
-            {
-                "role": "user",
-                "content": 
-                    "\"Very comfy bed large shower\","+
-                    "\"Our room is clean and the bed is so comfortable\""
-            },
-        ]
-    )
+    comment_clusters: list[str] = []
+    current_cluster: list[str] = []
+    curr_cluster_str_len = 0
+    for comment in comment_list:
+        current_cluster.append(comment)
+        curr_cluster_str_len += len(comment)
 
-    print(completion.choices[0].message.content)
+        if curr_cluster_str_len >= 2500 or len(current_cluster) >= 10:
+            comment_clusters.append(',\n'.join(current_cluster))
+            current_cluster.clear()
+            curr_cluster_str_len = 0
+    
+    if curr_cluster_str_len != 0:
+        comment_clusters.append(',\n'.join(current_cluster))
+        current_cluster.clear()
+        curr_cluster_str_len = 0
+
+    # for clustered in comment_clusters:
+    #     print(clustered)
+    #     print()
+
+    specific_instructions = {'pos': 'The comments are mostly positive, so treat ambiguous sentiments as positive', 'neg': 'The comments are mostly negative, so treat ambiguous sentiments as negative'}
+    if label_folder in specific_instructions:
+        main_instruction += f'\n{specific_instructions[label_folder]}'
+
+    openai.api_key = api_key
+    decoder = json.JSONDecoder()
+    labelled_resp = []
+
+    for i in range(len(comment_clusters)):
+        print(f'{i} / {len(comment_clusters)}')
+
+        try:
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": main_instruction
+
+                    },
+                    {
+                        "role": "user",
+                        "content": comment_clusters[i]
+                    },
+                ]
+            )
+
+            resp: str = completion.choices[0].message.content
+            labelled_resp.extend(decoder.decode(resp))
+
+            print(resp)
+        except:
+            print('request failed')
+            continue
+    
+    with open(f'{label_dir}/labelled.json', 'w+') as json_out:
+        json.dump(labelled_resp, json_out)
 
 
 if __name__ == '__main__':
+    config: dict[str, str]
     with open('./config.json', mode='r') as config_file:
         config = json.load(config_file)
-        API_KEY = config['API_KEY']
-    main()
+    label(api_key=config['API_KEY'], main_instruction=config['gpt_instruction'], label_folder='neg')
