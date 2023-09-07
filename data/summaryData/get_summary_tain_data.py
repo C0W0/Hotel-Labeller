fc1660ce83bd3884d754c49d748ebca8be5ffaa0
Terry <zhaterry@gmail.com>
2023-09-06 22:02:42 -0400

    used existing training data to augment new data

--- data/summaryData/get_summary_tain_data.py
+++ data/summaryData/get_summary_tain_data.py
@@ -5,10 +5,10 @@ from typing import Union
 
 sys.path.append(os.getcwd()+'\\')
 
-from utils import import_random_test_data, ASPECTS
+from utils import import_random_test_data, ASPECTS, ASPECTS_SUMMARY_ALIAS, should_ignore
 from data.labelled.labelling import label
 
-def label_using_gpt():
+def label_using_gpt(config: dict[str, str]):
     test_data = import_random_test_data()
 
     pos_sentiments: dict[str, list[dict[str, Union[float, str]]]] = {}
@@ -17,10 +17,7 @@ def label_using_gpt():
         pos_sentiments[aspect] = []
         neg_sentiments[aspect] = []
 
-    config: dict[str, str]
-    with open('./config.json', mode='r') as config_file:
-        config = json.load(config_file)
-    label_result = label(api_key=config['API_KEY'], comment_list=test_data, main_instruction=config['gpt_instruction'])
+    label_result = label(api_key=config['API_KEY'], comment_list=test_data, main_instruction=config['label_instruction'])
 
     for gpt_resp in label_result:
         try:
@@ -52,15 +49,42 @@ def use_existing_training_data():
     all_training_data: list[dict[str, Union[str, dict[str, float]]]] = {}
     with open('./data/labelled/neg/labelled.json') as labelled_file:
         all_training_data = json.load(labelled_file)
+    with open('./data/labelled/pos/labelled.json') as labelled_file:
+        all_training_data.extend(json.load(labelled_file))
     
-    neg_list: list[dict[str, Union[float, str]]] = []
+    pos_sentiments: dict[str, list[dict[str, Union[float, str]]]] = {}
+    neg_sentiments: dict[str, list[dict[str, Union[float, str]]]] = {}
+
+    
+    for aspect in ASPECTS_SUMMARY_ALIAS.keys():
+        pos_sentiments[aspect] = []
+        neg_sentiments[aspect] = []
+
     for comment_data in all_training_data:
         comment: str = comment_data['comment']
-        location_score: float = comment_data['labels']['Location']
-        if location_score < 0.35:
-            neg_list.append({ 'comment': comment,  'score': location_score })
-        else:
-            continue
+        for aspect in ASPECTS_SUMMARY_ALIAS.keys():
+            score = comment_data['labels'][aspect]
+            if score > 0.65:
+                pos_sentiments[aspect].append({ 'comment': comment,  'score': score })
+            if score < 0.35:
+                neg_sentiments[aspect].append({ 'comment': comment,  'score': score })
+            else:
+                continue
+
+    for aspect in ASPECTS_SUMMARY_ALIAS.keys():
+        with open(f'./data/summaryData/pos/{aspect}.json', mode='r') as file_json:
+            pos_sentiments[aspect].extend(json.load(file_json))
+        with open(f'./data/summaryData/neg/{aspect}.json', mode='r') as file_json:
+            neg_sentiments[aspect].extend(json.load(file_json))
     
-    with open('./data/summaryData/neg/Location.json', mode='w+') as file_json:
-        json.dump(neg_list, file_json, indent=2)
+    for aspect in ASPECTS_SUMMARY_ALIAS.keys():
+        with open(f'./data/summaryData/pos/{aspect}.json', mode='w+') as file_json:
+            json.dump(pos_sentiments[aspect], file_json, indent=2)
+        with open(f'./data/summaryData/neg/{aspect}.json', mode='w+') as file_json:
+            json.dump(neg_sentiments[aspect], file_json, indent=2)
+
+
+# config: dict[str, str]
+# with open('./config.json', mode='r') as config_file:
+#     config = json.load(config_file)
+use_existing_training_data()
